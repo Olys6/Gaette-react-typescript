fe908e9422acae4fa92f1a77e80246883ed5c6a8
Oliver Skjevesland <oliver@skjevesland.com>
2022-02-17 21:51:14 +0100

    trying to manage attempted guess

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ function App() {
     const form = event.target.form
     const index = [...form].indexOf(event.target)
 
-    
-
     setTimeout(() => {
       if(event.key === "Backspace") {
 
@@ -26,13 +24,24 @@ function App() {
         form.elements[index + 1].focus()
       } else {
         // guess1.push(form.element[index].value)
-        console.log(form.element[index].value)
+        setTimeout(() => {
+          console.log(form.element[index].value)
+        }, 1000)
+        
         console.log("GUESS ARRAY: ", guess1)
         form.elements[index + 1].focus()
 
       } 
       event.preventDefault()
     }, 1)
+  }
+
+  const handleGuess = (event: any) => {
+    if(event.target.value === "") {
+      guess1.pop()
+    }
+    guess1.push(event.target.value)
+    console.log(guess1)
   }
 
   let rowInputs = [1, 2, 3, 4]
@@ -43,7 +52,7 @@ function App() {
       <h3>guess the 4 letter word!</h3>
       <form className="App" >
         
-        <div className="row">{rowInputs.map((i) => <input key={i} onKeyDown={navigateInputs} className="RowInputs" id={`row1letter${i}`} type="text" maxLength={1} />)}</div>
+        <div className="row">{rowInputs.map((i) => <input key={i} onChange={handleGuess} onKeyDown={navigateInputs} className="RowInputs" id={`row1letter${i}`} type="text" maxLength={1} />)}</div>
 
         <div className="row">{rowInputs.map((i) => <input key={i} onKeyDown={navigateInputs} className="RowInputs" id={`row2letter${i}`} type="text" maxLength={1} />)}</div>
 
